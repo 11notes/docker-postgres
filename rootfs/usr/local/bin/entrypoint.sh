62e8920f4698038a9a69d51ce8f9c7b08139592f
ElevenNotes <github@11notes.ch>
2025-05-14 10:02:54 +0200

    [cut] remove cmd-socket

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -8,8 +8,6 @@
   fi
 
   if [ -z "${1}" ]; then
-    /usr/local/bin/cmd-socket -s /run/cmd/.sock &> /dev/null &
-
     if [ ! -z "$(ls -A ${APP_ROOT}/sql)" ]; then
       postgres --config-file=${APP_ROOT}/etc/default.conf &> /dev/null &
       eleven log info "executing SQL scripts ..."
@@ -19,7 +17,7 @@
       rm -rf ${APP_ROOT}/sql/*
       sleep 5
     fi
-
+    
     set -- postgres \
       --config-file=${APP_ROOT}/etc/default.conf
     eleven log start
