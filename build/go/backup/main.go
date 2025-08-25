cdc25fca04401ac1bf13c5d4f83a7de7df57b387
ElevenNotes <github@11notes.ch>
2025-08-25 14:26:33 +0200

    feat: check if destination already exists

--- build/go/backup/main.go
+++ build/go/backup/main.go
@@ -16,17 +16,15 @@ import (
 const SCHEDULE = "POSTGRES_BACKUP_SCHEDULE"
 
 func main(){
-	// syscalls
+	// catch syscalls
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, syscall.SIGTERM, syscall.SIGSTOP, syscall.SIGINT)
-
-	// event listener
 	go func() {
 		<- signalChannel
 		os.Exit(0)
 	}()
 
-	// set schedule
+	// set backup schedule
 	if _, ok := os.LookupEnv(SCHEDULE); ok {
 		fmt.Fprintf(os.Stdout, "setting schedule: %s\n", os.Getenv(SCHEDULE))
 		scheduler, err := gocron.NewScheduler()
@@ -45,31 +43,41 @@ func main(){
 }
 
 func backup(){
-	files := fmt.Sprintf("/postgres/backup/%s", time.Now().Format("20060102150405"))
-	cmd := exec.Command("pg_basebackup", "--compress","server-lz4","-D",files,"-cfast","-Xfetch","-Ft","-U", "postgres")
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid:true}
-	
-	stdout, _ := cmd.StdoutPipe()
-	stderr, _ := cmd.StderrPipe()
+	// create new path based on time stamp
+	backupPath := fmt.Sprintf("/postgres/backup/%s", time.Now().Format("20060102150405"))
 
-	go func() {
-		stdoutScanner := bufio.NewScanner(io.MultiReader(stdout,stderr))
-		for stdoutScanner.Scan() {
-			stdout := stdoutScanner.Text()
-			if ! strings.HasPrefix(stdout, "WARNING:  skipping special file"){
-				fmt.Println(stdout)
+	// check if destination exists already
+	if _, err := os.Stat(backupPath); os.IsNotExist(err){
+
+		// prepare pg_basebackup with LZ2 compression
+		cmd := exec.Command("pg_basebackup", "--compress","server-lz4","-D",backupPath,"-cfast","-Xfetch","-Ft","-U", "postgres")
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid:true}
+
+		stdout, _ := cmd.StdoutPipe()
+		stderr, _ := cmd.StderrPipe()
+
+		go func() {
+			stdoutScanner := bufio.NewScanner(io.MultiReader(stdout,stderr))
+			for stdoutScanner.Scan() {
+				stdout := stdoutScanner.Text()
+				if ! strings.HasPrefix(stdout, "WARNING:  skipping special file"){
+					fmt.Println(stdout)
+				}
 			}
+		}()
+
+		// start backup process
+		err := cmd.Start()
+		fmt.Printf("starting backup to [%s/base.tar.lz4]\n", backupPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "backup error: %s\n", err)
 		}
-	}()
 
-	err := cmd.Start()
-	fmt.Printf("starting backup to [%s/base.tar.lz4]\n", files)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "backup error: %s\n", err)
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "backup error: %s\n", err)
+		err = cmd.Wait()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "backup error: %s\n", err)
+		}
+	}else{
+		fmt.Fprintf(os.Stderr, "backup error: target %s exists already\n", backupPath)
 	}
 }
